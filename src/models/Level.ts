19ca75840138b9dca143c7b9b80ac0be2b1ada2f
Nick-2455 <nicolaspacheco2455@gmail.com>
2025-04-28 18:16:19 -0700

    Incluir gravedad en level

--- src/models/Level.ts
+++ src/models/Level.ts
@@ -8,6 +8,7 @@ interface LevelAttributes {
   name: string;
   description: string;
   expectedAnswer: number;
+  gravity: number;
   matchId: number;
 }
 
@@ -16,32 +17,27 @@ interface LevelCreationAttributes extends Optional<LevelAttributes, 'id'> {}
 @Table({
   tableName: 'Levels',
 })
+
 export class Level extends Model<LevelAttributes, LevelCreationAttributes> {
-  @Column({
-    type: DataType.STRING,
-    allowNull: false,
-  })
-  name!: string;
+    @Column({ type: DataType.STRING })
+    name!: string;
 
-  @Column({
-    type: DataType.TEXT,
-    allowNull: false,
-  })
-  description!: string;
+    @Column({ type: DataType.TEXT, allowNull: false })
+    description!: string;
 
-  @Column({
-    type: DataType.FLOAT,
-    allowNull: false,
-  })
-  expectedAnswer!: number;
+    @Column({ type: DataType.FLOAT, allowNull: false })
+    expectedAnswer!: number;
 
-  @ForeignKey(() => Match)
-  @Column
-  matchId!: number;
+    @Column({ type: DataType.FLOAT, allowNull: false }) 
+    gravedad!: number;
 
-  @BelongsTo(() => Match)
-  match!: Match;
+    @ForeignKey(() => Match)
+    @Column
+    matchId!: number;
 
-  @HasMany(() => Car)
-  cars!: Car[];
+    @BelongsTo(() => Match, { onDelete: "CASCADE" })
+    match!: Match;
+
+    @HasMany(() => Car)
+    cars!: Car[];
 }
