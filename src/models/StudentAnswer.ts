b7b320371a6ea37cfef81087afb9df82dfbf9348
Nick-2455 <nicolaspacheco2455@gmail.com>
2025-04-29 16:45:58 -0700

    Extrass

--- src/models/StudentAnswer.ts
+++ src/models/StudentAnswer.ts
@@ -1,31 +1,51 @@
-import { Table, Column, Model, DataType, CreatedAt, UpdatedAt } from 'sequelize-typescript';
+import { Table, Column, Model, DataType, ForeignKey, BelongsTo, CreatedAt, UpdatedAt } from 'sequelize-typescript';
+import { Optional } from "sequelize";
+import { Player } from "./Player"; 
+import { Level } from "./Level"; 
+
+interface StudentAnswerAttributes {
+  id: number;
+  playerId: number;
+  levelId: number;
+  answer: number;
+  score: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface StudentAnswerCreationAttributes extends Optional<StudentAnswerAttributes, 'id'> {}
 
 @Table({
-  tableName: 'student_answers',
+  tableName: 'student_answers', 
   timestamps: true,  
 })
-export class StudentAnswer extends Model {
+
+export class StudentAnswer extends Model<StudentAnswerAttributes, StudentAnswerCreationAttributes> {
   
-  @Column({ type: DataType.INTEGER.UNSIGNED, autoIncrement: true, primaryKey: true })
+  @Column({
+    type: DataType.INTEGER.UNSIGNED,
+    autoIncrement: true,
+    primaryKey: true
+  })
   id!: number;
 
-  @Column({ type: DataType.INTEGER.UNSIGNED})
+  @ForeignKey(() => Player)
+  @Column({ type: DataType.INTEGER.UNSIGNED })
   playerId!: number;
 
+  @ForeignKey(() => Level)
   @Column({ type: DataType.INTEGER.UNSIGNED })
   levelId!: number;
 
-  @Column({ type: DataType.FLOAT })
+  @Column({ type: DataType.FLOAT, allowNull: false })
   answer!: number;
 
-  @Column({ type: DataType.INTEGER })
+  @Column({ type: DataType.INTEGER, allowNull: false })
   score!: number;
 
-  @CreatedAt
-  @Column({ type: DataType.DATE })
-  createdAt!: Date;
+  @BelongsTo(() => Player)
+  player!: Player;
 
-  @UpdatedAt
-  @Column({ type: DataType.DATE })
-  updatedAt!: Date;
+  @BelongsTo(() => Level)
+  level!: Level;
 }
