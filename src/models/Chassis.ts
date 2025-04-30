fa9af477491612d0b72cb8ac8221c87887b75975
LuisP Cardenas <a01254955@tec.mx>
2025-04-29 23:26:21 -0700

    Merge the pending changes to the db model and resolve all conflicts [needs testing]

--- src/models/Chassis.ts
+++ src/models/Chassis.ts
@@ -1,7 +1,9 @@
 import { Table, Model, Column, DataType, ForeignKey, BelongsTo } from 'sequelize-typescript';
 import { Level } from './Level';
+import { Optional } from 'sequelize';
 
 interface ChassisAttributes {
+  id: number; 
   weight: number;              // peoso en kg
   distanceBetweenWheels: number;  // Distancia entre llantas en cm
   height: number;         // Dimensiones [width, height]
@@ -9,6 +11,7 @@ interface ChassisAttributes {
   assets: string[];          //! la imagen
 }
 
+interface ChassisCreationAttributes extends Optional<ChassisAttributes, 'id'>{}
 @Table({
   tableName: 'chasis'
 })
